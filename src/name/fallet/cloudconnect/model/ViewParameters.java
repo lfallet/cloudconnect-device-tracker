97ff548f6df2cdb2f1bff92a8fdaf555bc610af8
Laurent Fallet <laurent.fallet@gmail.com>
2012-07-22 16:50:04 +0200

    Présentation grisée des devices dont les informations ne datent pas du jour même.

---
+++ src/name/fallet/cloudconnect/model/ViewParameters.java
@@ -0,0 +1,22 @@
+package name.fallet.cloudconnect.model;
+
+/**
+ * Stocke les paramètres de la vue (coordonnées extrêmes à visualiser)
+ * 
+ * @author lfallet
+ */
+public class ViewParameters {
+
+	public int minLat = Integer.MAX_VALUE, maxLat = Integer.MIN_VALUE, minLng = Integer.MAX_VALUE, maxLng = Integer.MIN_VALUE;
+
+	/**
+	 * Etend les coordonnées limites si celles en entrées les dépassent
+	 */
+	public void extendsBoundariesIfNecessary(int lat, int lng) {
+		minLat = Math.min(minLat, lat);
+		maxLat = Math.max(maxLat, lat);
+		minLng = Math.min(minLng, lng);
+		maxLng = Math.max(maxLng, lng);
+	}
+
+}
